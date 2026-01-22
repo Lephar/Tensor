a033e322a39e475104d7dd4218de4131ddd607b0
Ali Emre Gülcü <aliemreglc@gmail.com>
2026-01-23 00:08:16 +0300

    Add dispatch functions

---
+++ app/src/main/cpp/src/dispatch.c
@@ -0,0 +1,15 @@
+#include "dispatch.h"
+
+#include "helper.h"
+
+void recordCommands() {
+    debug("Commands recorded");
+}
+
+void dispatch() {
+    debug("Commands dispatched");
+}
+
+void waitDispatch() {
+    debug("Dispatch finished");
+}
--- app/src/main/cpp/src/tensor.c
+++ app/src/main/cpp/src/tensor.c
@@ -3,6 +3,7 @@
 #include "device.h"
 #include "memory.h"
 #include "pipeline.h"
+#include "dispatch.h"
 
 JNIEXPORT void JNICALL
 Java_org_arch_tensor_MainActivity_initializeTensor(JNIEnv* env, jobject activity, jobject assets) {
@@ -18,6 +19,13 @@ Java_org_arch_tensor_MainActivity_initializeTensor(JNIEnv* env, jobject activity
     createShaderModules();
     createDescriptorSet();
     createPipeline();
+}
+
+JNIEXPORT void JNICALL
+Java_org_arch_tensor_MainActivity_runTensor(JNIEnv* env, jobject activity) {
+    recordCommands();
+    dispatch();
+    waitDispatch();
 }
 
 JNIEXPORT void JNICALL
